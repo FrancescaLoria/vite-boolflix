f6523318c3dced9561670e38ef50b930a0144a78
FrancescaLoria <francesca.loria90@hotmail.it>
2023-04-19 16:11:22 +0200

    basic structure

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: "AppHeader",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <header>
+    <div class="ms_searchbar">
+      <input type="text" placeholder="cerca" />
+    </div>
+  </header>
+</template>
+
+<style scoped lang="scss"></style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  name: "AppMain",
+};
+</script>
+
+<template>
+  <h1>main</h1>
+</template>
+
+<style scoped lang="scss"></style>
